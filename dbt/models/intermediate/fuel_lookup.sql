32fcdb5905f091acb80cebed718eee430fa070c0
mushroomsandchai <ajaymkatteo.0@gmail.com>
2026-04-03 13:35:50 +0000

    added energy type

--- dbt/models/intermediate/fuel_lookup.sql
+++ dbt/models/intermediate/fuel_lookup.sql
@@ -1,6 +1,20 @@
-{{ config(materialized = 'table') }}
+{{ config(materialized = 'view') }}
 select 
-    energy_source_code as fuel_id
-    energy_source_name as fuel
+    energy_source_code as fuel_id,
+    energy_source_name as fuel,
+    case
+        when energy_source_name in (
+            'Wind',
+            'Solar',
+            'Hydro and pumped storage',
+            'Geothermal',
+            'Wind with integrated battery storage',
+            'Solar with integrated battery storage',
+            'Battery storage',
+            'Pumped storage',
+            'Nuclear'
+        ) then 'renewable'
+        else 'non-renewable'
+    end as energy_type
 from
     {{ ref('energy_sources') }}
