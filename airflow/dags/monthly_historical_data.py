2b2817c977d40e80795e73e3d46c825efa512bcb
mushroomsandchai <ajaymkatteo.0@gmail.com>
2026-04-04 13:18:19 +0000

    removed dbt tasks from historical ingestion

--- airflow/dags/monthly_historical_data.py
+++ airflow/dags/monthly_historical_data.py
@@ -9,15 +9,10 @@ default_args = {
 @dag(
     dag_id = 'monthly_historical_data',
     description = """Fetch hourly electricity generation, interchange between balancing authorities,\
-                        demand forecast and demand by subregion data by month from EIA API and store in GCP bucket.\
-                        This dag does not load it to the data warehouse to be efficient with data warehouse's compute usage.\
-                        Once the end date i.e last day of Feb 28th of 2026 is met, it'll unpause the load_historical_data dag,\
-                        which loads our datalake data to the data warehouse. If we were to load the historical data by month,\
-                        we would inccur upwards of 10GB of byte usage from data warehouse.\
-                        This way, all the data gets loaded at once and is ready for daily consumption of API values.""",
+                        demand forecast and demand by subregion data by month from EIA API and store in GCP bucket.""",
     schedule = "@monthly",
     start_date = datetime(2019, 1, 1),
-    end_date = datetime(2026, 2, 28),
+    end_date = datetime(2019, 1, 31),
     catchup = True,
     max_active_runs = 4,
     default_args = default_args,
@@ -44,52 +39,16 @@ def main():
 
         upload_blob(buffer, gcp_file_path)
 
-    @task.short_circuit
-    def should_load_tables():
-        # Loads tables to big query if the total number of parquet files in the bucket is equal to 4 parquet files per month
-        # from start date to end date
-        context = get_current_context()
-        start_date = context['dag'].start_date.date()
-        end_date = context['dag'].end_date.date()
-        num_months = ((end_date.year - start_date.year) * 12) + ((end_date.month - start_date.month) + 1)
-        file_needed = num_months * 4
-
-        from helpers.file_count import count
-        count = count()
-
-        return file_needed == count
-
     @task
     def load(endpoint: dict):
         from helpers.load import load_table
         load_table(type = endpoint['type'], interval = 'batch_load')
 
 
-    from cosmos import DbtTaskGroup
-    from cosmos.config import RenderConfig
-    from helpers.dbt_configuration import dbt_objects
-
-    PROFILE, PROJECT, EXECUTION = dbt_objects()
-
-    dbt_models = DbtTaskGroup(
-        profile_config = PROFILE,
-        project_config = PROJECT,
-        execution_config = EXECUTION,
-        render_config = RenderConfig(
-            select = ["path:models/+"]
-        ),
-        operator_args={
-            "vars": {
-                "run_date": "{{ ds }}"
-            }
-        }
-    )
-
     endpoints = get_endpoints()
     ingestion = ingest.expand(endpoint = endpoints)
     loader = load.expand(endpoint = endpoints)
-    should_load = should_load_tables()
 
-    ingestion >> should_load >> loader >> dbt_models
+    ingestion >> loader
 
 main()
